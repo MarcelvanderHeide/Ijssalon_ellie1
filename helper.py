2e28e0be9d93c4bf57041b2e1b4977479fceee6b
MarcelvanderHeide <marcel.van.der.heide#enerpac.com>
2026-04-11 11:07:53 +0200

    Nieuwe lokale versie van de boekhouding

--- helper.py
+++ helper.py
@@ -1,7 +1,25 @@
 def decoreer(tekst=""):
+    tekst="header"
     lengte = len(tekst) + 4
     print()
     print(lengte * "*")
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, persoon):
+    bedrag_pp = bedrag/persoon
+    return f"het bedrag per persoon is {bedrag_pp} euro"
+
+def ondersteep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.appens(len/(tekst) * "=")
+    return uit
+
+
+def som(dictionary):
+    totaal = 0
+    for waarde in dictionary.values():
+        totaal += waarde
+    return totaal
